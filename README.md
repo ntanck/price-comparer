1480b77098acc7d905c0cace3bcf44be0415c2a9
Guilherme Francisco de Freitas <guifreitas89@hotmail.com>
2021-01-09 18:40:51 -0300

    Structure overhaul, uses API instead

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-# [Comparador de preço](https://gfrancisco.info/price-comparer/index.html)
-Ferramenta para facilitar a listagem de hardware de certas lojas.
+# Archiver Management Interface
 
-## Utilização
+## Build
 
-Apenas pesquise pelos termos que busca
+Para realização do build, use `npm install` para instalar as dependências do projeto, seguido de `npm run build` ou `npm run dev`.
 
-## Mais alguma coisa?
+## Dependências
 
-Se ver algo que não deveria acontecer, abra uma issue ou entre em contato. Para doações, clique no link abaixo
-
-## Donations / Doações
-
-[Doar](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CWC5F3ECE92JS&source=url)
+* **Papa Parse**: Utilizado para parsing de arquivos CSV
+* **jQuery**: Solução mais robusta e simples para várias funcionalidades JS
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import "./css/style.css"
+let $ = require("jquery");
+
+const expressions = require('./regex.json');
+
+document.getElementById("queryBox").onkeypress = function (event) {
+    if (event.keyCode == 13 || event.which == 13) {
+        var query = cleanUpSpecialChars(event.target.value);
+        var websites = Object.keys(expressions);
+        $('.p-column').empty();
+
+        websites.forEach(website => {
+            drawBoxes(website, query);
+        });
+    }
+};
+
+async function drawBoxes(store, query) {
+    $(`#${store}-title .lds-dual-ring`).show();
+    
+    console.log("AAAA");
+    $.ajax({
+        url: `https://hardware-scraper-api.herokuapp.com/get?store=${store}&query=${query}`,
+    })
+        .done(function (products) {
+            for (const p of products) {
+                console.log(p);
+                $("<a>", { class: "product", "href": p.url }).append(
+                    $("<img>", { class: "picture", src: p.img }),
+                    $("<h3>", { class: "name" }).text(p.name),
+                    $("<h3>", { class: "price" }).text(`R\$${p.price}`)
+                ).appendTo(`#${store}-column`);
+                $(`#${store}-title .lds-dual-ring`).hide();
+            };
+        });
+}
+
+function cleanUpSpecialChars(str) {
+    return str.toLowerCase()
+        .replace(/[&#039]/g, "'")
+        .replace(/[&quot;]/g, "\"")
+        .replace(/[óòôõö]/g, "o")
+        .replace(/[íìï]/g, "i")
+        .replace(/[úùü]/g, "u")
+        .replace(" ", "+")
+        .replace(/[^a-z0-9\+]/gi, '');
+}
+
+
